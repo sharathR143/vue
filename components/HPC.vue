178898f18001286961e726e4ee0dbc03125d27d9
sharath R <114079961+sharathR143@users.noreply.github.com>
2024-04-24 17:06:56 +0530

    Update HPC.vue

--- components/HPC.vue
+++ components/HPC.vue
@@ -1,5 +1,17 @@
 <template>
   <header class="header">
+                  <!-- service side -->
+     <footer class="container">
+      <h2 class="container-service">Service</h2>
+      <ul class="server-wrapper" v-for="server in serverdata" :key="server.id">
+        <li draggable="true" @dragstart="onDragStart($event)">
+          <p>{{ server.name }}</p>
+          <p>{{ server.server }}</p>
+        </li>
+      </ul>
+    </footer>
+
+            <!-- HPC AND VM AND SERVICE SIDE -->
     <div class="parent justify">
       <div class="content" v-for="(user, i) in users" :key="user.HPC">
         <div class="parent">
@@ -17,30 +29,31 @@
                 class="server-row"
                 :data-parent-index="i"
                 :data-current-index="j"
-                @click="vm.server !== '' ? showRemove($event) : ''"
-                draggable="false"
+                @click="vm.server !== '' ? showRemove($event) : '' "
+                @dblclick="vm.server !== '' ? showClosed($event) :'' "
+               
+                draggable="true"
                 @dragover="onDragOver($event)"
                 @drop="onDragDrop($event)"
                 @dragleave="onDragLeave($event)"
               >
-                {{ vm.server }}
+               <!-- <h3> </h3> -->
+               {{ vm.server }}
                 <span
-                  class="d-none remove-Button"
+                  class="d-none remove-Button svg"
                   @click="remove($event, i, j)"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    height="15"
                     viewBox="0 -960 960 960"
-                    width="15"
-                  >
+                    class="svg-class">
                     <path
                       d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
                     />
                   </svg>
                 </span>
               </div>
-              <div class="vm">{{ vm.name }}</div>
+              <div ><h3 class="vm">{{ vm.name }}</h3></div>
             </div>
           </div>
         </div>
@@ -50,24 +63,15 @@
       </div>
     </div>
 
-    <!-- service side -->
 
-    <footer class="container">
-      <h2 class="container-service">Service</h2>
-      <ul class="server-wrapper" v-for="server in serverdata" :key="server.id">
-        <li draggable="true" @dragstart="onDragStart($event)">
-          <p>periciption</p>
-          <p>{{ server.server }}</p>
-        </li>
-      </ul>
-    </footer>
+   
+
   </header>
 </template>
 
 <script setup>
 import userData from "./user.json";
 import serverdata from "./server.json";
-// import draggable from "vuedraggable";
 import { ref } from "vue";
 
 let users = ref(userData);
@@ -76,17 +80,23 @@ let draggedElementValue = ref("");
 
 const showRemove = (e) => {
   e.preventDefault();
-  let tragetElement = e.target.children[0];
-  tragetElement.classList.remove("d-none");
+  let targetElement = e.target.children[0];
+  targetElement.classList.remove("d-none");
 };
+const showClosed = (e) => {
+  e.preventDefault();
+  let targetElement = e.target.children[0];
+  targetElement.classList.add("d-none");
+};
+
 
 let remove = (e, parentIndex, childIndex) => {
   e.preventDefault();
-  let tragetElement = e.target.closest("div");
-  let tragetCloseTag = e.target.closest("span");
+  let targetElement = e.target.closest("div");
+  let targetCloseTag = e.target.closest("span");
   users.value[Number(parentIndex)].VM[Number(childIndex)].server = "";
-  tragetCloseTag.classList.add("d-none");
-  tragetElement.classList.remove("server-active");
+  targetCloseTag.classList.add("d-none");
+  targetElement.classList.remove("server-active");
 };
 
 const onDragStart = (e) => {
@@ -135,6 +145,23 @@ const onDragDrop = (e) => {
 .d-none {
   display: none;
 }
+.svg{
+  background-color: rgb(182, 196, 209);
+  border-radius: 10px 50px;
+  border-radius: 2px;
+  height: 18px;
+  width: 16px;
+}
+.svg-class{
+  height: 16px;
+  width: 16px;
+  margin-top: 1px;
+}
+.remove-Button {
+  position: absolute;
+  top: -20px;
+  right: -5px;
+}
 .parent {
   display: flex;
   flex-wrap: wrap;
@@ -145,18 +172,18 @@ const onDragDrop = (e) => {
   justify-content: center;
   border: 3px solid green;
   margin-top: 3%;
-  padding: 40px 0px;
-  width: 84%;
+  padding: 20px 0px;
+  width: 87%;
 }
 .content {
   margin-right: 5px;
   margin-left: 2px;
-  margin-bottom: 100px;
+  margin-bottom: 50px;
+  margin-top: 10px;
   padding-bottom: 100px;
 }
 .server div {
   height: 20px;
-  cursor: pointer;
 }
 .server-active {
   background-color: rgb(151, 234, 151);
@@ -169,35 +196,37 @@ const onDragDrop = (e) => {
   border: 1px dashed transparent;
   margin-bottom: 5px;
   position: relative;
-}
-.remove-Button {
-  position: absolute;
-  top: -17px;
-  right: 0px;
+  padding: 5px 3px;
+  cursor: pointer;
 }
 .service {
   background-color: rgb(151, 234, 151);
   text-align: center;
 }
 .vm {
-  background-color: gray;
-  padding: 6px 16px;
-  margin-top: 6px;
+  background-color: rgb(220, 209, 209);
+  padding: 6px 23px;
+  font-family:  Times, serif;
+  margin-top: 10px;
 }
 .hpc {
   background-color: rgb(240, 180, 102);
   text-align: center;
-  padding: 15px;
-  margin-top: 55px;
+  padding:17px 15px;
+  margin-top: 68px;
+  font-family:  Times, serif;
 }
 .container {
   border: 3px solid green;
-  width: 200px;
+  width: 180px;
+  background-color: blueviolet;
   margin-top: 42px;
-  overflow-y: auto;
+  /* overflow-y: auto; */
+  /* height: 600px; */
 }
 .container-service {
   text-align: center;
+  background-color: aqua;
 }
 
 .server-wrapper {
@@ -205,23 +234,18 @@ const onDragDrop = (e) => {
   flex-direction: column;
 }
 .server-wrapper li {
-  list-style: none;
-  background-color: rgb(151, 234, 151);
-  text-align: center;
-  padding: 6px 15px;
-  width: 100px;
-  height: auto;
-  cursor: pointer;
-}
-/* .server-user {
-  margin-bottom: 20px;
   display: flex;
-  justify-content: center;
-} */
-/* .service-right {
-  background-color: rgb(242, 41, 19);
+  justify-content:space-evenly;
+  background-color: rgb(208, 184, 158);
   text-align: center;
-  padding: 2px 0px;
-  width: 60px;
-} */
+  margin-bottom: 40px;
+  width: 130px;
+  cursor: pointer;
+  list-style: none;
+}
+.server-wrapper  p {
+  background-color: blueviolet; 
+  /* margin: 10px; */
+}
+
 </style>
