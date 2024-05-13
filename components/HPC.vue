bd201708860bc6fd098b175a356c2560d9f529aa
sharath R <114079961+sharathR143@users.noreply.github.com>
2024-05-13 22:38:11 +0530

    Update HPC.vue

--- components/HPC.vue
+++ components/HPC.vue
@@ -1,20 +1,32 @@
 <template>
   <header class="header">
-                  <!-- service side -->
-     <footer class="container">
+    <!-- service side -->
+    <footer class="container">
       <h2 class="container-service">Service</h2>
-     <!-- <div class="container-div"> -->
       <ul class="server-wrapper" v-for="server in serverdata" :key="server.id">
-        <li draggable="true" @dragstart="onDragStart($event)" class="li">
-          <a>{{ server.name }}</a>
-          <a>{{ server.server }}</a>
+        <li
+          draggable="true"
+          @dragstart="onDragStart($event)"
+          :style="{
+            backgroundColor:
+              server.color !== ''
+                ? server.color
+                : (server.color = generateRandomColors()),
+          }"
+        >
+          <!-- :style="{ backgroundColor: server.color }" -->
+          <!-- <p>{{ server.id++ }}</p> -->
+          <p>{{ server.name }}</p>
+          <p>{{ server.server }}</p>
         </li>
       </ul>
-     <!-- </div> -->
     </footer>
 
-            <!-- HPC AND VM AND SERVICE SIDE -->
-    <div class="parent justify">
+    <!-- HPC AND VM AND SERVICE SIDE -->
+    <div
+      class="parent"
+      :class="errorMessage === '' ? 'justify' : 'error-border'"
+    >
       <div class="content" v-for="(user, i) in users" :key="user.HPC">
         <div class="parent">
           <!-- <aside class="server" v-for="(vm, i) in user.VM" :key="i">
@@ -31,16 +43,16 @@
                 class="server-row"
                 :data-parent-index="i"
                 :data-current-index="j"
-                @dblclick="vm.server !== '' ? showRemove($event) : '' "
-                @click="vm.server !== '' ? showClosed($event) :'' "
-               
-                draggable="false"
+                @click="vm.server !== '' ? showRemove($event) : ''"
+                @dblclick="vm.server !== '' ? showClosed($event) : ''"
+                draggable="true"
                 @dragover="onDragOver($event)"
                 @drop="onDragDrop($event)"
                 @dragleave="onDragLeave($event)"
+                :style="{ backgroundColor: vm.backgroundColor }"
               >
-               
-               {{ vm.server }}
+                <!-- <h3> </h3> -->
+                {{ vm.server }}
                 <span
                   class="d-none remove-Button svg"
                   @click="remove($event, i, j)"
@@ -48,14 +60,17 @@
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 -960 960 960"
-                    class="svg-class">
+                    class="svg-class"
+                  >
                     <path
                       d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
                     />
                   </svg>
                 </span>
               </div>
-              <div ><h3 class="vm">{{ vm.name }}</h3></div>
+              <div>
+                <h3 class="vm">{{ vm.name }}</h3>
+              </div>
             </div>
           </div>
         </div>
@@ -64,11 +79,10 @@
         </main>
       </div>
     </div>
-
-
-   
-
   </header>
+  <div v-if="errorMessage !== ''">
+    <p>{{ errorMessage }}</p>
+  </div>
 </template>
 
 <script setup>
@@ -80,17 +94,27 @@ let users = ref(userData);
 let draggedElement = ref("");
 let draggedElementValue = ref("");
 
+let usedServices = 0;
+let errorMessage = ref("");
+
+function generateRandomColors() {
+  let color = "#" + Math.floor(Math.random() * 16777215).toString(16); // Generate random hex color
+  return color;
+}
+
 const showRemove = (e) => {
   e.preventDefault();
+  console.log(e.target);
+  console.log(e.target.children);
   let targetElement = e.target.children[0];
   targetElement.classList.remove("d-none");
 };
+
 const showClosed = (e) => {
   e.preventDefault();
   let targetElement = e.target.children[0];
   targetElement.classList.add("d-none");
 };
-
 
 let remove = (e, parentIndex, childIndex) => {
   e.preventDefault();
@@ -99,6 +123,8 @@ let remove = (e, parentIndex, childIndex) => {
   users.value[Number(parentIndex)].VM[Number(childIndex)].server = "";
   targetCloseTag.classList.add("d-none");
   targetElement.classList.remove("server-active");
+  usedServices--;
+  errorMessage.value = "";
 };
 
 const onDragStart = (e) => {
@@ -128,34 +154,48 @@ const onDragDrop = (e) => {
   let currentIndex = droppedElement.getAttribute("data-current-index");
 
   if (droppedElement.innerText === "") {
-    users.value[Number(parentIndex)].VM[Number(currentIndex)].server =
-      draggedElementValue;
-    droppedElement.classList.add("server-active");
-    droppedElement.classList.remove("service-dragover");
-    droppedElement.classList.add("server-row");
+    if (usedServices < 10) {
+      usedServices += 1;
+      console.log(draggedElement.style.backgroundColor);
+      let vmdata = users.value[Number(parentIndex)].VM[Number(currentIndex)];
+
+      vmdata.server = draggedElementValue;
+      vmdata.backgroundColor = draggedElement.style.backgroundColor;
+
+      droppedElement.classList.add("server-active");
+      droppedElement.classList.add("server-row");
+    } else {
+      // droppedElement.classList.remove("service-dragover");
+      // droppedElement.classList.add("server-row");
+      // console.log("error - service reached maximum limit");
+      errorMessage.value = "error - service reached maximum limit";
+    }
     // draggedElement.remove();
   }
+  droppedElement.classList.remove("service-dragover");
 };
 </script>
 
 <style scoped>
-
 .header {
   display: flex;
   justify-content: space-between;
-  height: 84vh;
+  height: 80vh;
+}
+.error-border {
+  border: 3px solid red !important;
 }
 .d-none {
   display: none;
 }
-.svg{
+.svg {
   background-color: rgb(182, 196, 209);
   border-radius: 10px 50px;
   border-radius: 2px;
   height: 18px;
   width: 16px;
 }
-.svg-class{
+.svg-class {
   height: 16px;
   width: 16px;
   margin-top: 1px;
@@ -175,105 +215,83 @@ const onDragDrop = (e) => {
   justify-content: center;
   border: 3px solid green;
   margin-top: 3%;
-  padding: 30px 0px;
+  padding: 10px 0px;
+  /* padding: 20px 0px; */
   width: 87%;
 }
 .content {
   margin-right: 5px;
   margin-left: 2px;
-  margin-bottom: 80px;
-  margin-top: 20px;
+  margin-bottom: 50px;
+  margin-top: 10px;
+  padding-bottom: 100px;
 }
 .server div {
   height: 20px;
-  padding: 6px 0px;
 }
 .server-active {
-  /* background-color: rgb(95, 172, 95); */
-  /* background-color: rgb(151, 212, 151); */
-  background-color: #e0aaff;
+  background-color: rgb(151, 234, 151);
   text-align: center;
 }
 .service-dragover {
-  border: 2px dashed green;
+  border: 1px dashed green;
 }
 .server-row {
   border: 1px dashed transparent;
-  margin-bottom: 4px;
-  font-size: 18px;
-  font-weight: 550;
+  margin-bottom: 5px;
   position: relative;
+  /* padding: 5px 3px; */
   cursor: pointer;
 }
 .service {
+  background-color: rgb(151, 234, 151);
   text-align: center;
+  /* margin-bottom: px; */
 }
 .vm {
-  /* background-color: rgb(220, 209, 209); */
-  background-color: rgb(195, 187, 187);
-  padding: 7px 23px;
-  font-family:  Times, serif;
-  font-size: 18px;
-  font-weight: 550;
-  margin-top: 1px;
+  background-color: rgb(220, 209, 209);
+  padding: 6px 23px;
+  font-family: Times, serif;
+  margin-top: 13px;
 }
 .hpc {
-  /* background-color: rgb(240, 180, 102); */
-   background-color: rgb(241, 196, 128); 
+  background-color: rgb(240, 180, 102);
   text-align: center;
-  padding:17px 15px;
-  font-size: 18px;
-  font-weight: 550;
-  margin-top: 65px;
-  font-family:  Times, serif;
+  padding: 17px 15px;
+  margin-top: 73px;
+  font-family: Times, serif;
 }
 .container {
   border: 3px solid green;
   width: 180px;
-  padding-right: 0px;
+  /* background-color: blueviolet; */
   margin-top: 42px;
-  /* background-color: rgb(191, 176, 206); */
   /* overflow-y: auto; */
   /* height: 600px; */
 }
-
-
-.server-wrapper {
-   display: flex;
-  flex-direction: column;
-  justify-content: start;
-
-}
-
 .container-service {
   text-align: center;
-  font-size: 28px;
+  /* background-color: aqua; */
 }
 
-
-.li{
+.server-wrapper {
   display: flex;
-  justify-content:space-between;
-  /* background-color: rgb(133, 204, 133); */
- /* background-color: #d29cdc; */
-  /* background-color: #d29cdc; */
-  background-color: rgb(151, 212, 151);
-  /* background-color: #e0aaff; */
-  border-radius: 1px;
-  margin-top: 10px;
-  padding-top: 2px;
-  margin-left: 0px;
-  padding-left: 4px;
-  padding-right: 1px;
-  align-items: center;
-  width: 110px;
-  height: 42px;
+  flex-direction: column;
+}
+.server-wrapper li {
+  display: flex;
+  justify-content: space-evenly;
+  background-color: rgb(133, 204, 133);
+  text-align: center;
+  /* margin-bottom: 20px; */
+  border-radius: 2px;
+  width: 120px;
   cursor: pointer;
   list-style: none;
 }
-
-.li a{
-  margin: 14px 5px;
+.server-wrapper p {
+  /* background-color: blueviolet; */
+  margin: 3px 0px;
+  padding: 10px 5px;
 }
-
 </style>
