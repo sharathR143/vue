20d6b726603d1c225b0b286611b72cee059659b8
sharath R <114079961+sharathR143@users.noreply.github.com>
2024-09-26 23:25:49 +0530

    Update HPC.vue

--- components/HPC.vue
+++ components/HPC.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div>
     <p class="text-[20px] ml-3">service locations</p>
     <aside>
@@ -28,7 +28,7 @@
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, computed } from "vue";
 import Servicelocation from "../components/servicelocation.vue";
 import Containerjson from "../data/container.json";
 
@@ -37,8 +37,54 @@ const hostnames = ["soc0", "soc1", "zcfront", "zcRear"];
 
 const filteredParkingData = ref([]);
 filteredParkingData.value = props.drivingjson;
+
+
+
 onMounted(() => {
   console.log("filteredParkingData", filteredParkingData);
   console.log("selected", props.selected);
 });
+</script> -->
+
+<template>
+  <div>
+    <p class="text-[20px] ml-3">service locations</p>
+    <aside class="flex justify-evenly mr-5">
+      <div class="flex gap-2 items-end bg-yellow-00 border-red-300 border-2">
+        <div v-for="host in hostnames" :key="host">
+          <template v-if="selected && filteredParkingData.length > 0">
+            <div v-for="ParkingData in filteredParkingData" :key="ParkingData">
+              <div class="mb-4" v-if="ParkingData.config['HostName'] == host">
+                <div
+                  v-for="name in ParkingData.names"
+                  :key="name"
+                  class="px-[20px] py-[10px] border-dashed border-[red] border-2"
+                >
+                  {{ name }}
+                </div>
+              </div>
+            </div>
+          </template>
+
+          <div class="px-[20px] py-[10px] border-dashed border-black border-2">
+            {{ host }}
+          </div>
+        </div>
+
+        <p v-if="selected && filteredParkingData.length === 0">
+          No parking data available.
+        </p>
+      </div>
+    </aside>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(["drivingjson", "selected"]);
+const hostnames = ["soc0", "soc1", "zcfront", "zcRear"];
+
+// Use computed to automatically update filteredParkingData based on props
+const filteredParkingData = computed(() => props.drivingjson);
 </script>
